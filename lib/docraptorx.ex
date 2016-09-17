bbeb609c08c1d0e724612b1139f2a7b85c09a564
asacraft <asacraftn@gmail.com>
2016-09-17 15:42:31 +0900

    configuration

--- lib/docraptorx.ex
+++ lib/docraptorx.ex
@@ -1,3 +1,8 @@
 defmodule Docraptorx do
   alias Docraptorx.HttpClient
+
+  def configure(api_key, base_url \\ "https://docraptor.com") do
+    Application.put_env(:docraptorx, :api_key, api_key)
+    Application.put_env(:docraptorx, :base_url, base_url)
+  end
 end
