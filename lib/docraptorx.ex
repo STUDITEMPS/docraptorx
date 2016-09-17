0aca5eea4cf607cc03ff872e926495936e6c8fbc
asacraft <asacraftn@gmail.com>
2016-09-17 21:03:31 +0900

    get status

--- lib/docraptorx.ex
+++ lib/docraptorx.ex
@@ -13,6 +13,15 @@ defmodule Docraptorx do
     end
   end
 
+  def status!(status_id) do
+    response = HttpClient.get!("/status/#{status_id}", %{})
+    if response.status_code === 200 do
+      {:ok, response.body}
+    else
+      {:error, response.body}
+    end
+  end
+
   def configure(api_key, base_url \\ "https://docraptor.com") do
     Application.put_env(:docraptorx, :api_key, api_key)
     Application.put_env(:docraptorx, :base_url, base_url)
