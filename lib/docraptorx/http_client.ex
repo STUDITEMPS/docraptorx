de3cb514f2cb1ec5455916f28c6df65dd5353404
asacraft <asacraftn@gmail.com>
2016-09-17 18:23:42 +0900

    http client with auth header

--- lib/docraptorx/http_client.ex
+++ lib/docraptorx/http_client.ex
@@ -1,3 +1,17 @@
 defmodule Docraptorx.HttpClient do
   use HTTPoison.Base
+
+  defp process_url(url) do
+    Application.get_env(:docraptorx, :base_url) <> url
+  end
+
+  defp process_request_headers(headers) when is_map(headers) do
+    headers
+    |> Map.put("Authorization", "Basic " <> auth_key)
+    |> Enum.into([])
+  end
+
+  defp auth_key do
+    Base.encode64("#{Application.get_env(:docraptorx, :api_key)}:")
+  end
 end
