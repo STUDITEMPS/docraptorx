731ccb8dff16e2715d25ad9c4f9818c04b01321d
asacraft <asacraftn@gmail.com>
2016-09-17 18:53:21 +0900

    create document

--- lib/docraptorx.ex
+++ lib/docraptorx.ex
@@ -1,6 +1,18 @@
 defmodule Docraptorx do
   alias Docraptorx.HttpClient
 
+  def create!(opts \\ %{}) do
+    body = JSX.encode!(opts)
+    headers = %{"Content-type": "application/json"}
+    response = HttpClient.post!("/docs", body, headers)
+    IO.inspect(response)
+    if response.status_code === 200 do
+      {:ok, response.body}
+    else
+      {:error, response.body}
+    end
+  end
+
   def configure(api_key, base_url \\ "https://docraptor.com") do
     Application.put_env(:docraptorx, :api_key, api_key)
     Application.put_env(:docraptorx, :base_url, base_url)
